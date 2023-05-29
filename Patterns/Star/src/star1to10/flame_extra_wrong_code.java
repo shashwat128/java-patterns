6887d63c5e9f1f565eceda39e4f1533ee58792f7
Shashwat Singh <shashwat2048@gmail.com>
2023-05-29 22:03:22 +0530

    java diamond

---
+++ Patterns/Star/src/star1to10/flame_extra_wrong_code.java
@@ -0,0 +1,29 @@
+package star1to10;
+
+public class flame_extra_wrong_code {
+    public static void main(String[] args) {
+        int rows = 5;
+        starPrint(rows);
+    }
+
+    private static void starPrint(int rows) {
+        for(int i = 1; i <= 2 * rows - 1; i++){
+            if(i <= rows) {
+                for (int k = i; k <= rows - i; k++){
+                    System.out.print(" ");
+                }
+                for (int k = 1; k <= i; k++) {
+                    System.out.print("*");
+                }
+            } else {
+                for (int k = 1; k <= i - rows; k++) {
+                    System.out.print(" ");
+                }
+                for (int k = 1; k <= 2 * rows - i; k++) {
+                    System.out.print("*");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
