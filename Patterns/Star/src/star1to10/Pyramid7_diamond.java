c82b4bb33f560fd0d1d592252017ba97956274bc
Shashwat Singh <shashwat2048@gmail.com>
2023-05-29 10:52:31 +0530

    java diamond

--- Patterns/Star/src/star1to10/Pyramid7_diamond.java
+++ Patterns/Star/src/star1to10/Pyramid7_diamond.java
@@ -2,6 +2,32 @@ package star1to10;
 
 public class Pyramid7_diamond {
     public static void main(String[] args) {
+        int rows = 5;
+        starPrint(rows);
+    }
 
+    private static void starPrint(int rows) {
+
+        // Upper half
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= rows - i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= 2 * i - 1; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        // Lower half
+        for (int i = rows - 1; i >= 1; i--) {
+            for (int j = 1; j <= rows - i; j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= 2 * i - 1; k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }
