2cd3d2c0246bdabd453034ea73efa04cd9481fff
Shashwat Singh <shashwat2048@gmail.com>
2023-05-31 18:06:57 +0530

    star pattern of

---
+++ Patterns/Star/src/star11to20/Pattern18_hollow_triangle_inverted_right.java
@@ -0,0 +1,25 @@
+package star11to20;
+
+public class Pattern18_hollow_triangle_inverted_right {
+    public static void main(String[] args) {
+        int rows = 5;
+        starPrint(rows);
+    }
+
+    private static void starPrint(int rows) {
+        for (int i = 1; i <= rows; i++ ) {
+            for (int k = 1; k < i; k++) {
+                System.out.print(" ");
+            }
+
+            for (int k = rows; k >= i; k--) {
+                if(i == 1 || k == i || k == rows) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
