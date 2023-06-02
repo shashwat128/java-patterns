1102b0e63b2c29ef59e0a0bdda149a2461d89d37
Shashwat Singh <shashwat2048@gmail.com>
2023-06-02 18:44:36 +0530

    inverted mirror hollow triangle wrong

---
+++ Patterns/Star/src/star21to30/Pattern28_hollow_triangle_inverted_mirror.java
@@ -0,0 +1,37 @@
+package star21to30;
+
+public class Pattern28_hollow_triangle_inverted_mirror {
+    public static void main(String[] args) {
+        int rows = 5;
+        starPrint(rows);
+    }
+
+    private static void starPrint(int rows) {
+        for (int i = 1; i <= rows; i++) {
+            for (int k = 1; k < i; k++) {
+                System.out.print(" ");
+            }
+            for (int k = i; k <= rows; k++) {
+                if (i == 1 || k == i || k == rows)
+                    System.out.print("* ");
+                else
+                    System.out.print(" ");
+            }
+            System.out.println();
+        }
+
+
+        for (int i = rows - 1; i >= 1; i--) {
+            for (int k = 1; k < i; k++) {
+                System.out.print(" ");
+            }
+            for (int k = i; k <= rows; k++) {
+                if(i == 1 || k == i || k == rows)
+                    System.out.print("* ");
+                else
+                    System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}
