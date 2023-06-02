0a08fefa00b65ace510db4fc4a181ce51d21a0ee
Shashwat Singh <shashwat2048@gmail.com>
2023-06-02 16:50:29 +0530

    star pattern of horizontal triangle hollow

---
+++ Patterns/Star/src/star21to30/Pattern22_hollow_right_horizontal_triangle.java
@@ -0,0 +1,36 @@
+package star21to30;
+
+public class Pattern22_hollow_right_horizontal_triangle {
+    public static void main(String[] args) {
+        int rows = 5;
+        starPrint(rows);
+    }
+
+    private static void starPrint(int rows) {
+        for(int i = 1; i <= rows; i++) {
+            for (int k = 1; k <= i; k++) {
+                if (k == 1 || k == i) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+
+        for (int i = 1; i <= rows - 1; i++ ) {
+            for (int k = rows - 1; k >= i; k--) {
+                if (k == rows - 1 || k == i || i == rows) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+
+            for (int k = 1; k < i; k++) {
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}
